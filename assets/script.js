1ad5de34732b2fefbbd6461fff65f762b60fb688
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-09-27 23:08:19 -0400

    made variables in js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var apiKey ="ef28a20c16a093868e24b53cf2c8f202"
+var SearchFormEl = document.getElementById('search-form')
+var citySearch = document.getElementById('city-search')
+var weatherApiUrl = "api.openweathermap.org/data/2.5/weather?q=London&appid="
+
